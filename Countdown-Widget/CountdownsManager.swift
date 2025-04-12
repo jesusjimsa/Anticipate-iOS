8d67df57e828dcc0a08e7d20115da7e08b0d1e9b
jesusjimsa <jesusjimsa@icloud.com>
2025-04-12 13:14:37 +0100

    Comment unnecessary code
    Will delete later

---
+++ Countdown-Widget/CountdownsManager.swift
@@ -0,0 +1,8 @@
+//
+//  CountdownsManager.swift
+//  Countdown-Widget
+//
+//  Created by Jesús Jiménez Sánchez on 11/4/25.
+//
+
+import Foundation
---
+++ Countdowns/CountdownEntity.swift
@@ -0,0 +1,8 @@
+//
+//  CountdownEntity.swift
+//  Countdown-Widget
+//
+//  Created by Jesús Jiménez Sánchez on 12/4/25.
+//
+
+import Foundation
